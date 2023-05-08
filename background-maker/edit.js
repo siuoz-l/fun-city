78eb2a966df31d67e1df6f6f7b94be30b2dc37d9
SiuozlOrganisation <107283393+SiuozlOrganisation@users.noreply.github.com>
2023-05-08 09:21:42 -0400

    .

---
+++ background-maker/edit.js
@@ -0,0 +1,4 @@
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    localStorage.setItem("SIUOZL_12100_header", document.getElementById("SIUOZL_12100_header").value);   
+};
