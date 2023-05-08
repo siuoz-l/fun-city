d48d3d1d985797b549261c1e73ba5dd520a68dc7
SiuozlOrganisation <107283393+SiuozlOrganisation@users.noreply.github.com>
2023-05-08 09:35:21 -0400

    .

--- background-maker/edit.js
+++ background-maker/edit.js
@@ -1,4 +1,8 @@
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
     localStorage.setItem("SIUOZL_12100_header", document.getElementById("SIUOZL_12100_header").value);   
+    var userName = localStorage.getItem("Siuozl_ProfileNAME");
+            window.onload = function() {
+                document.getElementById("userName").innerText = (userName)
+            };
 };
